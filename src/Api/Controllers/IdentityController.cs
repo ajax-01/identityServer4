40f6755430e41a4c5235048b6c2197ffc9fc5d5f
yzf <498484538@qq.com>
2020-09-23 17:15:20 +0800

    sfs

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
